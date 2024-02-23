b78590c1c4b025b97bf77ff7a74dda7616f98708
EgorSivenko <egor.siv20@gmail.com>
2024-02-23 14:35:53 +0200

    Updated `Note` entity

--- src/main/java/org/example/todolist/entity/Note.java
+++ src/main/java/org/example/todolist/entity/Note.java
@@ -1,13 +1,15 @@
 package org.example.todolist.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Note {
@@ -16,9 +18,4 @@ public class Note {
     private String title;
     private String content;
     private LocalDateTime lastUpdatedOn;
-
-    public Note(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
 }
