0615f2cfbcf5c9fbc40d1cc1c621ab04e5d5f49b
EgorSivenko <egor.siv20@gmail.com>
2024-02-24 19:55:37 +0200

    Added and configured Spring DATA JPA + PostgreSQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     compileOnly("org.projectlombok:lombok")
