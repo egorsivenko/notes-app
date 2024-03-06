a402a46d8cd5eb986f6ae017e6d0f4db9a36221e
EgorSivenko <egor.siv20@gmail.com>
2024-03-06 17:55:16 +0200

    Implemented 'User' and 'Role' repositories

---
+++ src/main/java/org/example/todolist/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package org.example.todolist.repository;
+
+import org.example.todolist.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
---
+++ src/main/java/org/example/todolist/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.example.todolist.repository;
+
+import org.example.todolist.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+}
