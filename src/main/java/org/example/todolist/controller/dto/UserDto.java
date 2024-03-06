3406782093d3fb46ff830106a963cf86c91470e9
EgorSivenko <egor.siv20@gmail.com>
2024-03-06 20:59:59 +0200

    Implemented AuthController for user authentication

---
+++ src/main/java/org/example/todolist/controller/dto/UserDto.java
@@ -0,0 +1,23 @@
+package org.example.todolist.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 255)
+    private String password;
+}
--- src/main/java/org/example/todolist/security/SecurityConfig.java
+++ src/main/java/org/example/todolist/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(jpaUserDetailsService)
