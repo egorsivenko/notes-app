3d45aba0b77bef321cdc3b264f38eae4493daf0e
EgorSivenko <egor.siv20@gmail.com>
2024-03-14 13:31:02 +0200

    Added additional details for the authentication token

--- src/main/java/org/example/todolist/security/jwt/JwtAuthFilter.java
+++ src/main/java/org/example/todolist/security/jwt/JwtAuthFilter.java
@@ -8,6 +8,7 @@ import org.example.todolist.security.JpaUserDetailsService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -39,6 +40,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
 
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
