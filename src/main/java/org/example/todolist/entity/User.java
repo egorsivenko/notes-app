0c94cd4d276c31d921d3cae473d93deea85151df
EgorSivenko <egor.siv20@gmail.com>
2024-04-13 15:34:50 +0300

    Updated user and role id type

--- src/main/java/org/example/todolist/entity/User.java
+++ src/main/java/org/example/todolist/entity/User.java
@@ -29,7 +29,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false, unique = true)
     private String username;
